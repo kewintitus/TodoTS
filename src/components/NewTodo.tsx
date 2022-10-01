eefbe2a41b394aa4e5fadd25f0d9faf265e39fc9
Kewin Titus <kewintitus@gmail.com>
2022-10-01 20:34:28 +0530

    [FEAT]- added Todo app logic

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,27 @@
+import { useRef } from 'react';
+
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      return;
+    }
+
+    props.onAddTodo(enteredText);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="text">Todo text</label>
+      <input ref={todoTextInputRef} type="text" id="text"></input>
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
